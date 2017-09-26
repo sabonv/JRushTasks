5421aabdee6f55664c00b32c6751b71c17c36df5
sabonv <znakomiymne@list.ru>
2017-09-27 02:08:08 +0300

    dom

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1713/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task17.task1713;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+/* 
+Общий список
+*/
+
+public class Solution implements List<Long>{
+    private ArrayList<Long> original = new ArrayList<Long>();
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
