f40419ea2fc5f6eb750b998e74b57c605281556a
sabonv <znakomiymne@list.ru>
2017-10-17 18:01:16 +0300

    work, after update plugin JRush

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task20.task2025;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -9,50 +10,48 @@ import java.util.List;
 Алгоритмы-числа
 */
 public class Solution {
-    public static long[] getNumbers(long N) {
 
-        long[] result = null;
+    public static int[] getNumbers(long N) {
 
-        List<Long> listL = new ArrayList<>();
+        List<Integer> listN = new ArrayList<>();
 
-        for (long i = N; i >= 0; i--) {
+        for (int i = 0; i <= N; i++) {
+            int length = 1;
+            if(i != 0) length = (int) (Math.log10(i) + 1);
 
-           int m = String.valueOf(i).length();
-           double res = 0;
-           char[] mArr = String.valueOf(i).toCharArray();
+            if(length == 1) listN.add(i);
 
-            for (Character chr: mArr) {
-                String temp = String.valueOf(chr);
-                res = res + Math.pow(Integer.parseInt(temp), m);
+            else if (length == 2) {
+
             }
 
-            if(i == res) {
-                listL.add(i);
-            }
+
+
 
         }
 
-        result = new long[listL.size()];
-        for (int i = 0; i < listL.size(); i++) {
-            result[i] = listL.get(i);
-        }
-
-        return result;
+        return null;
     }
 
     public static void main(String[] args) {
 
         long startDate = new Date().getTime();
+
         //System.out.println(startDate);
-        long[] out = getNumbers(1000000000);
+
+
+        int[] out = getNumbers(380);
 
         for (long vot: out) {
             System.out.print(vot + " ");
         }
-        long stopDate = new Date().getTime();
+
+
+
+
         System.out.println();
-        System.out.println((stopDate - startDate));
+        System.out.println((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/1024/1024 + " MB");
+        System.out.println((new Date().getTime()- startDate));
+
     }
 }
-//http://rextester.com/KHEI65875
-//http://www.cyberforum.ru/java-j2se/thread1743615.html
