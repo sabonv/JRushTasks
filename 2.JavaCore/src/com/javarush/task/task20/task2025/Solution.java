0b75e10416e167f50b5fa3a4ef3879f314092222
sabonv <znakomiymne@list.ru>
2017-10-18 01:09:17 +0300

    dom

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,10 +1,7 @@
 package com.javarush.task.task20.task2025;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /*
 Алгоритмы-числа
@@ -14,24 +11,109 @@ public class Solution {
     public static int[] getNumbers(long N) {
 
         List<Integer> listN = new ArrayList<>();
+        List<Integer> lisiTest = new ArrayList<>();
 
+        int count = 0;
+        Set<Integer> setAN = new TreeSet<>();
         for (int i = 0; i <= N; i++) {
-            int length = 1;
-            if(i != 0) length = (int) (Math.log10(i) + 1);
-
-            if(length == 1) listN.add(i);
-
-            else if (length == 2) {
-
+            if(isNumberUnique(i)) {
+                listN.add(i);
+                count++;
+                int firstN = summAM(i);
+                if(!setAN.contains(firstN)) {
+                    if (isANumber(firstN) && firstN <= N) lisiTest.add(firstN);
+                }
             }
-
-
-
-
         }
+
+        //-----------
+        System.out.println(listN);
+        //System.out.println(listN.size());
+
+//        List<Integer> listAN = new ArrayList<>();
+//
+//
+//        for (Integer testN: listN) {
+//            int firstN = summAM(testN);
+//            if(!setAN.contains(firstN)) {
+//                if (isANumber(firstN) && firstN <= N) setAN.add(firstN);
+//            }
+//        }
+        System.out.println(count);
+        System.out.println(lisiTest);
+        System.out.println(setAN);
 
         return null;
     }
+
+
+    private static boolean isANumber(int number) {
+
+        int lenth = (number == 0) ? 1 : (int) Math.ceil(Math.log10(Math.abs(number) + 0.5));
+
+        int summ = 0;
+
+        List<Integer> tempArInt = new ArrayList<>();
+        if (number > 0){
+            for(int a=number; a>0; a/=10)
+                tempArInt.add(0, a%10);
+        }else tempArInt.add(0);
+
+        for (int i = 0; i < lenth; i++) {
+            summ = summ + (int)Math.pow(tempArInt.get(i), lenth);
+        }
+
+        return number == summ;
+    }
+
+    private static int summAM(int number) {
+        boolean test = false;
+        int lenth = (number == 0) ? 1 : (int) Math.ceil(Math.log10(Math.abs(number) + 0.5));
+
+        List<Integer> tempArInt = new ArrayList<>();
+        if (number > 0){
+            for(int a=number; a>0; a/=10)
+                tempArInt.add(0, a%10);
+        }else tempArInt.add(0);
+
+        int summ = 0;
+
+        for (int i = 0; i < lenth; i++) {
+            summ = summ + (int)Math.pow(tempArInt.get(i), lenth);
+        }
+        int testPont = 0;
+        return summ;
+    }
+
+    private static boolean isNumberUnique(int number) {
+        int lenth = (number == 0) ? 1 : (int) Math.ceil(Math.log10(Math.abs(number) + 0.5));
+        List<Integer> tempArInt = new ArrayList<>();
+        if (number > 0){
+            for(int a=number; a>0; a/=10)
+                tempArInt.add(0, a%10);
+        }
+
+        if(lenth == 1) return true;
+        else if(lenth == 2) {
+
+            if(tempArInt.get(0) <= tempArInt.get(1)) return true;
+        }
+
+        else{ //100
+            boolean test = false;
+
+            for (int i = 1; i < lenth-1; i++) {
+                if(((tempArInt.get(i)>tempArInt.get(i-1)) &&(tempArInt.get(i)<tempArInt.get(i+1))) || tempArInt.get(lenth-1) == 0)
+                    test = true;
+                else test = false;
+            }
+            return test;
+
+        }
+
+        return false;
+    }
+
 
     public static void main(String[] args) {
 
@@ -39,19 +121,25 @@ public class Solution {
 
         //System.out.println(startDate);
 
-
-        int[] out = getNumbers(380);
-
+        //146511209
+        //9800817 s8
+        //9474
+        int[] out = getNumbers(1000);
+/*
+        List<Integer> check = new ArrayList<>();
+        for(int a=110; a>0; a/=10)
+            check.add(0, a%10);
+        System.out.println(check + "!");
+*/
+/*
         for (long vot: out) {
             System.out.print(vot + " ");
         }
-
-
-
+*/
 
         System.out.println();
         System.out.println((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/1024/1024 + " MB");
-        System.out.println((new Date().getTime()- startDate));
+        System.out.println((new Date().getTime()- startDate) / 1000 + " s.");
 
     }
 }
