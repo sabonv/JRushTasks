e24a6fb5d73399e4e8e668ce31c0ca1b56e19ae9
sabonv <znakomiymne@list.ru>
2017-10-31 08:05:09 +0300

    dom

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task24.task2413;
+
+public class Arkanoid {
+    private int width;
+    private int height;
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
