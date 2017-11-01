ad0f63875b1752185b2ccd708e6a5f62bc84edaa
sabonv <znakomiymne@list.ru>
2017-11-01 18:00:08 +0300

    work

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by v.usov on 01.11.2017.
+ */
+public interface Item {
+    int getId();
+    double getPrice();
+    String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by v.usov on 01.11.2017.
+ */
+public interface Jeans extends Item {
+
+    int getLength();
+    int getSize();
+
+}
