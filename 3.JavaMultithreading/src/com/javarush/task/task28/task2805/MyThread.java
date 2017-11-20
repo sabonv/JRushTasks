bbbbbc8d82490bec369de9134b33630cc8efe361
sabonv <znakomiymne@list.ru>
2017-11-20 23:44:10 +0300

    dom

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -7,43 +7,78 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class MyThread extends Thread {
 
-    public static AtomicInteger priorT = new AtomicInteger(0);
-    public AtomicInteger priorG = new AtomicInteger(this.getThreadGroup().getMaxPriority());
+    public static AtomicInteger priorT = new AtomicInteger(1);
+    //public AtomicInteger priorG = new AtomicInteger(this.getThreadGroup().getMaxPriority());
 
     public MyThread() {
-        if (priorT.get() >= 10) priorT.set(0);
-        this.setPriority(priorT.incrementAndGet());
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
 
     }
 
     public MyThread(Runnable target) {
         super(target);
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 
     public MyThread(ThreadGroup group, Runnable target) {
         super(group, target);
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 
     public MyThread(String name) {
         super(name);
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 
     public MyThread(ThreadGroup group, String name) {
         super(group, name);
-        if (priorT.get() >= 10) priorT.set(0);
-        if (priorT.get() >= priorG.get()) priorT.set(priorG.get() - 1);
-        this.setPriority(priorT.incrementAndGet());
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 
     public MyThread(Runnable target, String name) {
         super(target, name);
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 
     public MyThread(ThreadGroup group, Runnable target, String name) {
         super(group, target, name);
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 
     public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
         super(group, target, name, stackSize);
+        if (priorT.get() < 10) setPriority(priorT.getAndIncrement());
+        else {
+            setPriority(priorT.get());
+            priorT.set(1);
+        }
     }
 }
