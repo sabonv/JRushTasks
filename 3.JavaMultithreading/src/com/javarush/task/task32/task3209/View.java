a19a7d53398e3bb2abed30ae456d8dfaf0e71c83
sabonv <znakomiymne@list.ru>
2017-11-27 01:38:06 +0300

    dom - 8 level BIG(6) not done

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,12 +14,46 @@ import java.awt.event.ActionListener;
 public class View extends JFrame implements ActionListener {
 
     private Controller controller;
-
-
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public void init(){
+
+        initGui();
+        this.addWindowListener(new FrameListener(this));
+        this.setVisible(true);
+
+    }
+
+    public void selectedTabChanged(){
+
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+
+    }
+
+    public void initGui() {
+
+        initMenuBar();
+        initEditor();
+        pack();
+
+    }
+
+    public void exit(){
+        controller.exit();
     }
 
     public Controller getController() {
