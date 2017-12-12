9b15540fc91e7d170889c3cd6934676c5621c159
sabonv <znakomiymne@list.ru>
2017-12-12 17:56:19 +0300

    work - 10 level BIG(1) done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by v.usov on 12.12.2017.
+ */
+public class Controller {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by v.usov on 12.12.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
